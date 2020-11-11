a4a86dc720fb37fbdd3c201606b354b1dd451f55
Samantha <samanthagottlieb31@gmail.com>
2020-11-11 18:56:03 +0000

    Create post request in app.rb, create views files, add feature test

--- app.rb
+++ app.rb
@@ -2,7 +2,13 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
   get '/' do
-    'Testing infrastructure working!'
+    erb(:index)
+  end
+
+  post '/names' do
+    @p_1 = params[:name_1]
+    @p_2 = params[:name_2]
+    erb(:play)
   end
 
   run! if app_file == $0
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,8 +1,17 @@
 require_relative '../../app.rb'
 
 describe Battle, type: :feature do
-  it "displays 'Testing infrastructure working!' on homepage" do
+  it "Who will be going to battle?" do
     visit '/'
-    expect(page).to have_content('Testing infrastructure working!')
+    expect(page).to have_content("Who will be going to battle?")
+  end
+
+  it 'allows players to input names, submit form and see names on screen' do
+    visit '/'
+    fill_in('name_1', with: 'Constantine')
+    fill_in('name_2', with: 'Samantha')
+    click_button('Submit')
+    expect(page).to have_content('Constantine')
+    expect(page).to have_content('Samantha')
   end
 end
