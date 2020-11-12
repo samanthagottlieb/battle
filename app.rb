789ec683a14af497125d09e03e4faf3dda922b95
Samantha <samanthagottlieb31@gmail.com>
2020-11-12 14:21:23 +0000

    Add sessions, add redirect

--- app.rb
+++ app.rb
@@ -1,13 +1,21 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @p_1 = params[:name_1]
-    @p_2 = params[:name_2]
+    session[:name_1] = params[:name_1]
+    session[:name_2] = params[:name_2]
+    redirect('/play')
+  end
+
+  get '/play' do
+    @p_1 = session[:name_1]
+    @p_2 = session[:name_2]
     erb(:play)
   end
 
